50b289c1ad6c23732a8cce560dd1aae034ab73e3
gui.ljy <gui.ljy@zempublic.co.kr>
2023-06-29 20:25:04 +0900

    카드 재질 표현코드 추가

--- src/card.js
+++ src/card.js
@@ -9,7 +9,12 @@ import * as THREE from 'three'
 class Card {
   constructor({ width, height, color }) {
     const geometry = new THREE.PlaneGeometry(width, height)
-    const meterial = new THREE.MeshStandardMaterial({ color })
+    const meterial = new THREE.MeshStandardMaterial({
+      color,
+      side: THREE.DoubleSide, //기본은 단면만 렌더링되므로 양면렌더링 추가하는 코드
+      roughness: 0.5,
+      metalness: 0.5,
+    })
 
     const mesh = new THREE.Mesh(geometry, meterial)
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GUI } from 'lil-gui'
 import Card from './card.js'
 
 window.addEventListener('load', function() {
@@ -6,6 +8,7 @@ window.addEventListener('load', function() {
 })
 
 function init() {
+  const gui = new GUI()
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true, // 배경 투명하게 처리하는 코드
@@ -29,6 +32,8 @@ function init() {
 
   camera.position.z = 25
 
+  const controls = new OrbitControls(camera, renderer.domElement)
+
   const card = new Card({
     width: 10,
     height: 15.8,
@@ -37,11 +42,35 @@ function init() {
 
   scene.add(card.mesh)
 
+  const cardFolder = gui.addFolder('Card')
+
+  cardFolder
+    .add(card.mesh.material, 'roughness')
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name('material.roughness')
+
+  cardFolder
+    .add(card.mesh.material, 'metalness')
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name('material.metalness')
+
   const ambientLight = new THREE.AmbientLight(0xffffff, .8)
 
   ambientLight.add(ambientLight)
 
   scene.add(ambientLight)
+
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, .6)
+  const directionalLight2 = directionalLight1.clone()
+
+  directionalLight1.position.set(1, 1, 3)
+  directionalLight2.position.set(-1, 1, -3)
+
+  scene.add(directionalLight1, directionalLight2)
 
   render()
 
